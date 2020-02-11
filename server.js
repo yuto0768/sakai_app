7392418a8955aa355302aec6279e6376f36eca8d
yuto sakai <yuto0768@gmail.com>
2020-02-11 21:21:59 +0900

    2/11 sever構築

---
+++ server.js
@@ -0,0 +1,5 @@
+var http = require("http");
+var express = require("express");
+var app = express();
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
