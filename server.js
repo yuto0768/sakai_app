3fd556c7e6881996678f503d44955678d730b768
yuto sakai <yuto0768@gmail.com>
2020-02-11 21:41:55 +0900

    トップページ作成

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var http = require("http");
 var express = require("express");
 var app = express();
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.use("/public", express.static("./public"));
+app.get("/", (req, res)=>{
+    res.render("index.ejs");//使用する変数を第２引数としてかく
+  });
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
