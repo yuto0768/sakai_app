0e88353ca6ab8404e4329afddbc512d2d8400737
yuto sakai <yuto0768@gmail.com>
2020-02-15 00:22:24 +0900

    top_pageに変更

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = express();
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.use("/public", express.static("./public"));
 app.get("/", (req, res)=>{
-    res.render("index.ejs");//使用する変数を第２引数としてかく
+    res.render("top_page.ejs");//使用する変数を第２引数としてかく
   });
 app.listen(80, () => console.log('Example app listening on port 80!'))
