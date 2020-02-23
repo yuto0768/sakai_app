71694b9b25caeb561a6dd157f81ecc7f6aef826f
yuto sakai <yuto0768@gmail.com>
2020-02-23 22:53:44 +0900

    レイアウト

--- server.js
+++ server.js
@@ -6,4 +6,20 @@ app.use("/public", express.static("./public"));
 app.get("/", (req, res)=>{
     res.render("top_page.ejs");//使用する変数を第２引数としてかく
   });
+
+app.get("/touroku", (req, res)=>{
+    res.render("touroku_page.ejs");//使用する変数を第２引数としてかく
+  });
+
+  app.get("/login", (req, res)=>{
+    res.render("login_page.ejs");//使用する変数を第２引数としてかく
+  });
+
+  app.get("/products", (req, res)=>{
+    res.render("products_page.ejs");//使用する変数を第２引数としてかく
+  }); 
+
+  app.get("/products/details", (req, res)=>{
+    res.render("details_page.ejs");//使用する変数を第２引数としてかく
+  });  
 app.listen(80, () => console.log('Example app listening on port 80!'))
