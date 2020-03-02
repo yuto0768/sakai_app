97f16d1534d58d8f6d19fa8e43f55da184e165cb
yuto0768 <55009385+yuto0768@users.noreply.github.com>
2020-03-02 22:39:08 +0900

    3/2時点

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 var http = require("http");
 var express = require("express");
 var app = express();
+
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.use("/public", express.static("./public"));
-app.get("/", (req, res)=>{
-    res.render("top_page.ejs");//使用する変数を第２引数としてかく
-  });
+app.set("view engine", "ejs")
 
-app.get("/touroku", (req, res)=>{
-    res.render("touroku_page.ejs");//使用する変数を第２引数としてかく
-  });
+app.get("/", (req, res) => {
+  res.render("top_page.ejs");//使用する変数を第２引数としてかく
+});
 
-  app.get("/login", (req, res)=>{
-    res.render("login_page.ejs");//使用する変数を第２引数としてかく
-  });
+app.get("/touroku", (req, res) => {
+  res.render("touroku_page.ejs");//使用する変数を第２引数としてかく
+});
 
-  app.get("/products", (req, res)=>{
-    res.render("products_page.ejs");//使用する変数を第２引数としてかく
-  }); 
 
-  app.get("/products/details", (req, res)=>{
-    res.render("details_page.ejs");//使用する変数を第２引数としてかく
-  });  
+app.get("/login", (req, res) => {
+  res.render("login_page.ejs");//使用する変数を第２引数としてかく
+});
+
+app.use(require("express-ejs-layouts"))
+
+app.get("/products/details", (req, res) => {
+  res.render("details_page.ejs");//使用する変数を第２引数としてかく
+});
+
+app.use(require("express-ejs-layouts"))
+
+app.get("/products", (req, res) => {
+  res.render("products_page.ejs", { layout: "layout_login.ejs" });//使用する変数を第２引数としてかく
+});
+
+app.get("/products/details/purchase", (req, res) => {
+  res.render("purchase.ejs", { layout: "layout_login.ejs" });//使用する変数を第２引数としてかく
+});
+
+
+
 app.listen(80, () => console.log('Example app listening on port 80!'))
+
