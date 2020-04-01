584eff74a1c2a09a193e1cf10c857c8a1692ecc8
yuto0768 <55009385+yuto0768@users.noreply.github.com>
2020-04-01 22:35:33 +0900

    delete 追加

--- router/admin.js
+++ router/admin.js
@@ -6,6 +6,17 @@ async function getProducts(req, res) {
     res.render("admin/list.ejs", { rows });
 }
 
+async function getProduct(req, res, id) {
+    let row = await Product.findOne({ where: { id: id } })
+    res.render("admin/edit.ejs", { row });
+}
+
+
+async function deleteProduct(req, res, id) {
+    await Product.destroy({ where: { id: id } })
+    res.redirect("/admin");
+}
+
 router.get("/", (req, res) => {
     getProducts(req, res)
 });
@@ -15,11 +26,11 @@ router.get("/add", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-    res.render("admin/edit.ejs"); //使用する変数を第２引数としてかく
+    getProduct(req, res, req.params.id)
 });
 
 router.post("/:id/delete", (req, res) => {
-    res.redirect("/admin");
+    deleteProduct(req, res, req.params.id)
 });
 
 module.exports = router;
