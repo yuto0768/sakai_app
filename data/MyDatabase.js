3b03e0bbf14c5c0bbeab4d107d163b783137fdfb
Toshihiro SEKIOKA <esper776@hotmail.com>
2020-04-27 22:14:36 +0900

    テーブルの関連付けを追記しました。

--- data/MyDatabase.js
+++ data/MyDatabase.js
@@ -137,19 +137,47 @@ const Purchase = sequelize.define('purchase', {
     timestamps: true　 //行作成日時更新日時自動追加
 });
 
-Purchase.belongsTo(User)
-User.hasMay(Purchase)
+//PurchaseとProductを1:n(purchase:product)で関連付ける
+//これにより、productテーブルにpurchaseId列が追加されます
+//PurchaseはUserに所属する（Userは複数のPurchaseを保持できる）という意味です
+Purchase.belongsTo(User);
+//Userは複数のPurchaseを保持するという意味です。上記とセットになります
+User.hasMany(Purchase);
+
+//PurchaseとProductをn:1(purchase:product)で関連付けます
+//これにより、PurchaseテーブルにproductId列が追加されます
+//1件の購入履歴に複数の製品を関連付ける場合は、n:nの関連付けにします。
+//n:n関連付けについては、1:1関連付けでの購入履歴ができてから、説明します。
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
 
 async function setup() {　　 //force:trueでデータ全削除
     await sequelize.sync({ force: true }) //await:promiseの短縮(then実行で次へ)
         // テスト用のユーザーを1件登録
     let tanaka = await User.create({ name: "tanaka", idType: IdType.Mail, password: "pass" })
-    await Product.create({ name: "Converse", color: "RED,WHITE,BLACK", size: "24.5cm,25.0cm,25.5cm,26.0cm,26.5cm,27.0cm", info: "aabcde", image: "32060180.jpg", price: "8800" })
+    let converse = await Product.create({ name: "Converse", color: "RED,WHITE,BLACK", size: "24.5cm,25.0cm,25.5cm,26.0cm,26.5cm,27.0cm", info: "aabcde", image: "32060180.jpg", price: "8800" })
     await Product.create({ name: "Converse1", color: "WHITE", size: "25.0cm", info: "aabcde", image: "32060180.jpg", price: "8800" })
     await Product.create({ name: "Converse2", color: "BLACK", size: "25.5cm", info: "aabcde", image: "32060180.jpg", price: "8800" })
     await Product.create({ name: "Converse3", color: "BLACK", size: "26.0cm", info: "aabcde", image: "32060180.jpg", price: "8800" })
     await Product.create({ name: "Converse4", color: "BLACK", size: "26.5cm", info: "aabcde", image: "32060180.jpg", price: "8800" })
     await Product.create({ name: "Converse5", color: "BLACK", size: "27.0cm", info: "aabcde", image: "32060180.jpg", price: "8800" })
+
+    //関連付けのサンプルコード
+    //田中さんがConverseを買ったというデータになります
+    await Purchase.create({ userId: tanaka.id, productId: converse.id })
+
+    //PurchaseとProduct、Userをjoinして、結果を表示します。
+    let purchases = await Purchase.findAll({
+        include: [
+            { model: User, required: true },
+            { model: Product, required: true }
+        ],
+        where: { userId: tanaka.id }
+    })
+    console.log(`田中さんの購入履歴は${purchases.length}件あります`)
+    for (p of purchases) {
+        console.log(`購入者:${p.user.name} 品名:${p.product.name}`)
+    }
 }
 
 test()
