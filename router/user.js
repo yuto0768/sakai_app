90afe4df56d99c93ca018d5ba6dc9e6af8a04fd2
yuto0768 <55009385+yuto0768@users.noreply.github.com>
2020-05-07 21:28:12 +0900

    ユーザー購入履歴確認追加

--- router/user.js
+++ router/user.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 var { Product, Purchase, User } = require("../data/MyDatabase");
 
-router.get("/user_history", async(req, res) => {
+router.get("/", async(req, res) => {
     let rows = await Purchase.findAll({
         include: [
             { model: User, required: true },
             { model: Product, required: true }
         ]
     })
-    res.render("/user_history.ejs", { rows })
+    res.render("user_history.ejs", { rows })
 });
 
 module.exports = router;
